c5fa7d8d34119b51cac71c70c5b316fd085f1288
ninoNebieridze <ninonbrd13@gmail.com>
2024-12-26 08:51:17 -0800

    we are gonaa max this one

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("marii");
         System.out.println("tornike");
+        System.out.println("nino");
+        System.out.println("mari");
+        System.out.println("tekla");
     }
 }
