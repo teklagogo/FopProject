c0ad64e4225c06db204bfcaddbe895fc3dccbc5d
tornike23 <tornike.toradze23@gmail.com>
2024-12-26 20:36:05 +0400

    sdfhsdf

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
-import java.sql.SQLOutput;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hdfhhhhhh");
+        sdsfgsfg
 
     }
 }
